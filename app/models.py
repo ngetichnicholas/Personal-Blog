add1f554fecf2944f764fe219052d183dee5964b
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-19 11:20:19 +0300

    Add User class model  functions

--- app/models.py
+++ app/models.py
@@ -18,3 +18,25 @@ class User(UserMixin,db.Model):
   bio = db.Column(db.String(255))
   profile_pic_path = db.Column(db.String())
   blogs = db.relationship('Blog', backref='user', lazy='dynamic')
+
+  @property
+  def set_password(self):
+    raise AttributeError('You cannot read the password attribute')
+
+  @set_password.setter
+  def password(self, password):
+    self.secure_password = generate_password_hash(password)
+
+  def verify_password(self, password):
+    return check_password_hash(self.secure_password,password) 
+  
+  def save_u(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+  
+  def __repr__(self):
+    return f'User {self.username}'
