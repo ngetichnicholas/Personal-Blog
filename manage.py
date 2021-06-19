b126a0505f21b2cb012ab4fbdd787bf74db0500e
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-19 10:10:38 +0300

    Update manage

--- manage.py
+++ manage.py
@@ -1,12 +1,17 @@
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app import create_app,db
+from app.models import User
 
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
 
+@manager.shell
+def make_shell_context():
+    return dict(app = app, db = db, User = User)
+    
 @manager.command
 def test():
     import unittest
