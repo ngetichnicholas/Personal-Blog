9677427c7e9e6f05f998eb505d357552b447a0b2
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-19 10:05:24 +0300

    Update app init

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ class Config:
   MAIL_USE_TLS = True
   MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
   MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+  
   #PHOTOS UPLOAD CONFIGURATION
   UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
