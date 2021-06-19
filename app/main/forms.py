eb038d9b0c67a310d3361c0fd617f42facbd6d8c
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-19 16:06:27 +0300

    Add class to create blog

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, TextAreaField, SubmitField
-from wtforms.validators import Required
+from flask_wtf.file import FileField,FileAllowed
+from wtforms import StringField,TextAreaField, SubmitField,ValidationError
+from wtforms.validators import Required,Email
+from flask_login import current_user
+from ..models import User
 
 class UpdateProfile(FlaskForm):
+  username = StringField('Enter Your Username', validators=[Required()])
+  email = StringField('Email Address', validators=[Required(),Email()])
   bio = TextAreaField('Write a brief bio about you.',validators = [Required()])
-  submit = SubmitField('Save')
+  profile_pic_path = FileField('profile picture', validators=[FileAllowed(['jpg','png'])])
+  submit = SubmitField('Update')
+
+class CreateBlog(FlaskForm):
+  title = StringField('Title',validators=[Required()])
+  description = TextAreaField('Blog Description',validators=[Required()])
+  content = TextAreaField('Blog Content',validators=[Required()])
+  submit = SubmitField('Post')
