87dbc3e0e76bd8f29e5ebd6ff31f02fc444a260a
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-06-21 16:08:14 +0300

    Production

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,4 @@
-from flask import render_template,url_for,redirect,request
+from flask import render_template,url_for,flash,redirect,request
 from . import auth
 from flask_login import login_user,login_required,logout_user
 from .forms import RegForm,LoginForm
@@ -14,6 +14,8 @@ def login():
     if user != None and user.verify_password(form.password.data):
       login_user(user,form.remember.data)
       return redirect(request.args.get('next') or url_for('main.index'))
+    flash('Invalid username or Password')
+
   return render_template('auth/login.html', loginform = form)
 
 @auth.route('/logout')
